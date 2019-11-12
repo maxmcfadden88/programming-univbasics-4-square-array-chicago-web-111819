38fd86ab4577efd6c7f2b49f184e4010b044b821
Max M <maxmcfadden88@gmail.com>
2019-11-12 19:15:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,9 @@
 def square_array(array)
-  counter = 0
-  while counter < array.length do 
-    array[]**
-    counter+=
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
 end
